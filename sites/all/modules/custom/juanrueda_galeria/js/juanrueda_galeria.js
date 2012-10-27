254f2c36ec053121bebf09cb1b75cee50f1a5905
Ury Parra <ury.letty@gmail.com>
2012-10-27 18:24:25 -0500

    CAmbios

--- sites/all/modules/custom/juanrueda_galeria/js/juanrueda_galeria.js
+++ sites/all/modules/custom/juanrueda_galeria/js/juanrueda_galeria.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.juanruedaGaleria = {
     attach: function(context, settings) {
       $('#carousel').elastislide({
-        imageW    : Drupal.settings.galeria.view_mode,
-        minItems  : 3,
+        imageW    : 3,
+        minItems  : Drupal.settings.galeria.view_mode,
         margin    : 2,
         border    : 0,
         current   : 12
