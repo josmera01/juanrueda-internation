5705e71ec4ef19d04d0a64a7a9be95c5622d798f
josmera <josmera01@gmail.com>
2012-10-28 17:54:46 -0500

    Modulo eventos

--- sites/all/modules/custom/juanrueda_eventos/juanrueda_eventos.js
+++ sites/all/modules/custom/juanrueda_eventos/juanrueda_eventos.js
@@ -9,9 +9,10 @@
 
         var myArray = contenido.split('/');
 
-        url = '/eventos/' + myArray[5]
-        $('#result-eventos').load(eventos, function() {
+        url = '/eventos/' + myArray[5];
 
+        $('#result-eventos').load(eventos, function() {
+          alert('hola');
         });
 
         return false;
