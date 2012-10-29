e7f560f48ab1d4f23e90c537e75114c07b3a03ab
josmera <josmera01@gmail.com>
2012-10-28 23:02:45 -0500

    Modulo custom response

--- sites/all/modules/custom/juanrueda_response/juanrueda_response.js
+++ sites/all/modules/custom/juanrueda_response/juanrueda_response.js
@@ -6,20 +6,34 @@
     attach: function(context) {
       $('body').bind('responsivelayout', function(e, d) {
         if($(this).hasClass("responsive-layout-mobile")) {
+          //menu principal
           $('#block-menu-menu-menu-secciones-principales .content ul').hide();
-            if ($('.menu-render').val() == 'Menu principal') {
+            if ($('.menu-render').val() == 'Menu') {
               $('.menu-render').css('display', 'block');
             }else{
-              $('#block-menu-menu-menu-secciones-principales').prepend('<div class="menu-render">Menu principal</div>');
+              $('#block-menu-menu-menu-secciones-principales').prepend('<div class="menu-render">Menu</div>');
             }
 
           $('.menu-render').click(function(){
             $('#block-menu-menu-menu-secciones-principales .content ul').toggle();
           });
+
+          $('#zone-footer .grid-12').hide();
+
+          if ($('#zone-footer').val() == 'Menu') {
+              $('#zone-footer').css('display', 'block');
+          }else{
+            $('#zone-footer').prepend('<div class="mapa-sitio-render">Mapa sitio</div>');
+          }
+
         }
         else {
           $('#block-menu-menu-menu-secciones-principales .content ul').css('display', 'block');
           $('.menu-render').hide();
+
+          $('#zone-footer .grid-12').css('display', 'block');
+          $('.mapa-sitio-render').hide();
+
         }
       });
   }
