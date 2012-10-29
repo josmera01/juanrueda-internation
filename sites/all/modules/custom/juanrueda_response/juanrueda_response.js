89f8ed65a5e5edb41ee0de82237b7e1d0b0bb9b7
josmera <josmera01@gmail.com>
2012-10-28 21:50:56 -0500

    Modulo custom response

--- sites/all/modules/custom/juanrueda_response/juanrueda_response.js
+++ sites/all/modules/custom/juanrueda_response/juanrueda_response.js
@@ -1,9 +1,16 @@
 (function($) {
+
+  Drupal.juan_rueda_respnse = Drupal.juan_rueda_respnse || {};
+
   Drupal.behaviors.juan_rueda_respnse = {
     attach: function(context) {
       $('body').bind('responsivelayout', function(e, d) {
         if($(this).hasClass("responsive-layout-mobile")) {
-          console.log("a");
+          $('#block-menu-menu-menu-secciones-principales ul').hide();
+          $('#block-menu-menu-menu-secciones-principales ul').append('<div class="menu-render">Menu principal</div>');
+          $('.menu-render').click(function(){
+            $('#block-menu-menu-menu-secciones-principales ul').toggle();
+          });
         }
       });
   }
