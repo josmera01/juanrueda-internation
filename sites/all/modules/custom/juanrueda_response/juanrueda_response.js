0803dc069fce0d890561a29348fa664622cae096
josmera <josmera01@gmail.com>
2012-10-28 21:17:38 -0500

    Modulo custom response

---
+++ sites/all/modules/custom/juanrueda_response/juanrueda_response.js
@@ -0,0 +1,8 @@
+(function($) {
+  Drupal.behaviors.juan_rueda_respnse = {
+    attach: function(context) {
+      if($("body").hasClass("responsive-layout-mobile")) {
+        alert("Yes, it has!");
+      }
+  };
+})(jQuery);
