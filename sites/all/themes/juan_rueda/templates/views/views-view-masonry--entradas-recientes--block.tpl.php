2c68b651660ab68817c207ee5299e1faaf5f4933
josmera <josmera01@gmail.com>
2012-10-30 00:20:56 -0500

    Cambios vista

--- sites/all/themes/juan_rueda/templates/views/views-view-masonry--entradas-recientes--block.tpl.php
+++ sites/all/themes/juan_rueda/templates/views/views-view-masonry--entradas-recientes--block.tpl.php
@@ -4,13 +4,15 @@
  * Default view template to display items in a jQuery Masonry grid.
  */
 ?>
-
+<?php $i = 0;?>
 <?php foreach ($rows as $id => $row):
   $clase = $view->result;
-  dpm($clase[0]->field_field_tipo_articulo);
-  $nombre_clase = 'categoria-' . $clase['field_field_tipo_articulo'][0]['raw']['tid'];
+  dpm($clase[$i]->field_field_tipo_articulo);
+  $nombre_clase = 'categoria-' . $clase[$i]->field_field_tipo_articulo[0]['raw']['tid'];
   ?>
   <div class="<?php print $nombre_clase ?> masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
     <?php print $row; ?>
   </div>
-<?php endforeach; ?>
+<?php
+$i++;
+ endforeach; ?>
