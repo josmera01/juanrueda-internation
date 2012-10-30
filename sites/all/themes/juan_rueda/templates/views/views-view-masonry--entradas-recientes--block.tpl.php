0bd7ff4ea6eb37ac63d93b2bb7c72db675558b9f
josmera <josmera01@gmail.com>
2012-10-30 00:18:01 -0500

    Cambios vista

--- sites/all/themes/juan_rueda/templates/views/views-view-masonry--entradas-recientes--block.tpl.php
+++ sites/all/themes/juan_rueda/templates/views/views-view-masonry--entradas-recientes--block.tpl.php
@@ -7,7 +7,7 @@
 
 <?php foreach ($rows as $id => $row):
   $clase = $view->result;
-  dpm($clase);
+  dpm($clase->field_field_tipo_articulo);
   $nombre_clase = 'categoria-' . $clase['field_field_tipo_articulo'][0]['raw']['tid'];
   ?>
   <div class="<?php print $nombre_clase ?> masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
