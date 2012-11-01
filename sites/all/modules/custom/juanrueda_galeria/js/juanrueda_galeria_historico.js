47d7db8b6eb11e9db3d63a3cc45f1bbb58ec4f0d
josmera <josmera01@gmail.com>
2012-11-01 00:30:09 -0500

    Cambios galeria

---
+++ sites/all/modules/custom/juanrueda_galeria/js/juanrueda_galeria_historico.js
@@ -0,0 +1,19 @@
+(function($){
+
+  Drupal.juanruedaCamiones = Drupal.juanruedaCamiones || {};
+
+  Drupal.behaviors.juanruedaCamiones = {
+    attach: function(context, settings) {
+
+      $('#carousel-historico').elastislide({
+        imageW    : 150,
+        minItems  : 3,
+        margin    : 5,
+        border    : 0,
+        current   : 8
+      });
+
+
+    }
+  }
+})(jQuery);
