b7e22e25d09f4b8b30944a59dff05b4577b47036
josmera <josmera01@gmail.com>
2012-11-05 10:22:09 -0500

    Cambios

--- sites/all/modules/custom/juanrueda_response/juanrueda_responseIE.js
+++ sites/all/modules/custom/juanrueda_response/juanrueda_responseIE.js
@@ -4,15 +4,11 @@
 
   Drupal.behaviors.juan_rueda_IE = {
     attach: function(context) {
-      $('body').bind('responsiveIE', function(e, d) {
-        if($(this).hasClass("responsive-layout-mobile")) {
-          //$('body').addClass('responsive-layout-wide');
+      $('body').bind('responsive_ie', function(e, d) {
+        if ($(this).hasClass("responsive-layout-mobile")) {
           alert('hola');
         }
-      }
-
-
       });
   }
-
+}
 })(jQuery);
