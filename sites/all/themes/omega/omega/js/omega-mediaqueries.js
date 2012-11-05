4c2a49199c4b16c9b15dd3b61bfb7b725dea5e38
josmera <josmera01@gmail.com>
2012-11-05 10:12:34 -0500

    Cambios

--- sites/all/themes/omega/omega/js/omega-mediaqueries.js
+++ sites/all/themes/omega/omega/js/omega-mediaqueries.js
@@ -17,7 +17,7 @@ Drupal.omega = Drupal.omega || {};
   var setCurrentLayout = function (index) {
     index = parseInt(index);
     previous = current;
-    current = Drupal.settings.omega.layouts.order.hasOwnProperty(index) ? Drupal.settings.omega.layouts.order[index] : 'wide';
+    current = Drupal.settings.omega.layouts.order.hasOwnProperty(index) ? Drupal.settings.omega.layouts.order[index] : 'mobile';
 
     if (previous != current) {
       $('body').removeClass('responsive-layout-' + previous).addClass('responsive-layout-' + current);
