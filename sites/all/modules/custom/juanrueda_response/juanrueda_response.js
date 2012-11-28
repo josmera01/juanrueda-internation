d225cfcd4d6c04142dbe6858dee5bf04d123bdc4
josmera <josmera01@gmail.com>
2012-11-27 21:20:42 -0500

    JS

--- sites/all/modules/custom/juanrueda_response/juanrueda_response.js
+++ sites/all/modules/custom/juanrueda_response/juanrueda_response.js
@@ -13,9 +13,8 @@
         posicion1 = posicion.replace('/', "");
         posicion2 = posicion1.replace('/', ",");
         new_text = posicion2.split(',');
-        console.log(new_text[0]);
-        console.log(new_text[1]);
-        var i = IsNumeric(new_text[0]) + ',' + IsNumeric(new_text[1]);
+
+        var i = new_text[0]) + ',' + new_text[1];
         alert(i)
         Drupal.gmap.getMap('gmap-auto1map-gmap0').map.setCenter(new google.maps.LatLng(i));
         Drupal.gmap.getMap('gmap-auto1map-gmap0').map.setZoom(10);
