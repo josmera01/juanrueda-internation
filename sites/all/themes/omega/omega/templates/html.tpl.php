149e9b2a92bed2be961ed2f5ae80bda5a68e86d2
josmera <josmera01@gmail.com>
2013-03-09 18:07:20 -0500

    Cambios

--- sites/all/themes/omega/omega/templates/html.tpl.php
+++ sites/all/themes/omega/omega/templates/html.tpl.php
@@ -2,10 +2,11 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 7]><style type="text/css" media="all">@import url("http://juanruedaconinternational.com/sites/all/themes/juan_rueda/css/ie7.css");</style><![endif]-->
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
